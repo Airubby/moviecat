c86497c0e0026acee8f3a1c46447765ff6d1a63e
Airubby <ai_rubby@sina.cn>
2016-11-21 00:28:32 +0800

    常规方法和自定义指令实现左侧导航焦点

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
 
 	angular.module('moviecat',[
 	'ngRoute',
-	'moviecat.public_list'
+	'moviecat.public_list',
+	'moviecat.directives.auto_focus'
 	])
 	
 	.config(['$routeProvider',function($routeProvider){
@@ -11,5 +12,25 @@
 			redirectTo:'/in_theaters/1'
 		})
 	}]);
+	
+	
+	/*
+	.controller('NavController',['$scope','$location',function($scope,$location){
+		
+		$scope.$location=$location;
+		$scope.$watch('$location.path()',function(now){
+			
+			if(now.startsWith('/in_theaters')){
+				$scope.type='in_theaters';
+			}else if(now.startsWith('/coming_soon')){
+				$scope.type='coming_soon';
+			}else if(now.startsWith('/top250')){
+				$scope.type='top250';
+			}
+			
+		});
+		
+	}]);
+	*/
 
 })(angular);
